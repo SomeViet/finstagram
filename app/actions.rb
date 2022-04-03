6f82dbdfa2383ccdb782f14f6203394a3d7f3922
Kevin <kevtran@hotmail.com>
2022-04-03 19:32:29 +0000

    W5D1

--- app/actions.rb
+++ app/actions.rb
@@ -6,6 +6,12 @@
 #         View ------- Controller
 #
 
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end
+
 
 
 get '/' do
@@ -33,10 +39,46 @@ post '/signup' do
 
     # if user validations pass and user is saved. ActiveRecord creates a record in the db
     if @user.save
-        "User #{username} saved!"
-
+        redirect to ('/login')
+        
     else
         erb(:signup)
     end
 
+end
+
+get '/login' do          #when a GET request comes into /login
+    erb(:login)         #render app/views/login.erb
+end
+
+post '/login' do         #when we submit a form with an action of /login
+    #params.to_s         #just play the params for now to make sure it's working
+
+    username = params[:username]
+    password = params[:password]
+
+    #1. find user by usernamee
+
+    @user = User.find_by(username: username)
+
+    #2 if that user exists and check if that user's password in db matches password input
+    if @user && @user.password == password
+        
+        session[:user_id] = @user.id
+        redirect to ('/')
+
+        #3. if maasswords match, login
+        
+           # "Success! User with id #{session[:user_id]} is logged in!"
+        else
+            @error_message = "Login Failed!"
+            erb(:login)
+        end 
+    
+end
+
+get '/logout' do
+    session[:user_id] = nil
+    redirect to ('/')
+    #"Logout Successful"
 end
