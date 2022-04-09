edcf259b772b04a822fffbe65c6b3e6340a9c761
Kevin <kevtran@hotmail.com>
2022-04-09 22:31:35 +0000

    W6D1

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,10 @@
 class Comment < ActiveRecord::Base
 
+    #associations to a user and post
     belongs_to :user
     belongs_to :finstagram_post
     
+    #validation code
+    validates_presence_of :text, :user, :finstagram_post
+
 end
